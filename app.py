74ceb48050050d6321691b2fff1e2a41a484a104
Shoaib175 <shaikhshoaib175@gmail.com>
2021-01-03 14:25:22 +0530

    My first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+
+def main():
+    st.title('On-Page SEO Optimizer')
+    st.subheader('Combining data science with SEO to get better results!')
+
+if __name__ == '__main__':
+    main()
